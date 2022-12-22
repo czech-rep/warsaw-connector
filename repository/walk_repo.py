0a4c567da461e14286cfc4578a6a4e117c3e08e5
czech-rep <adam.a.czech@proton.me>
2022-12-22 14:34:59 +0100

    cache departures

--- repository/walk_repo.py
+++ repository/walk_repo.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass, field
 import datetime
 import utils
 from repository import models, alchemy_tables
@@ -5,34 +6,116 @@ from repository.models import TableData, BusStop
 from sqlalchemy.future import select
 from sqlalchemy.orm import joinedload, selectinload
 from repository import session
-from typing import Iterable
+from typing import Iterable, Set
+from functools import lru_cache
+from typing import Dict, List
+from utils import add_minutes_to_hour
 
 
-def get_sibling_bus_stops(bus_stop: BusStop):
+MIN_AHEAD = 40
+MIN_BACK = -6
+MINIMUM_MIN_BUFFER = 18
+
+
+@lru_cache(3000)
+def get_bus_stops_of_same_unit(bus_stop: BusStop):
     q = select(BusStop).filter(BusStop.stop_id == bus_stop.stop_id)
     return session.execute(q).scalars()
 
 
 def get_outgoing_tables(
-    bus_stop_ids, time: datetime.time, min_ahead: int = 60
+    bus_stop_ids, time: datetime.time, unique_routes:bool, min_ahead: int = 60
 ) -> Iterable[TableData]:
     q = (
         select(TableData)
         .filter(TableData.bus_stop.in_(bus_stop_ids))
         .filter(TableData.time >= time)
         .filter(TableData.time < utils.add_minutes_to_hour(time, min_ahead))
-        .order_by(TableData.time)
+        .order_by(TableData.route, TableData.time)
     )
+    if unique_routes:
+        # we just need distinct routes, bc different direction means different route
+        q = q.distinct(TableData.route)
     return session.execute(q).scalars()
 
 
+def count_called(fn):
+    counter = 1
+
+    def inner(*a, **k):
+        nonlocal counter
+        counter += 1
+        if counter % 400 == 0:
+            print("- ", counter)
+        return fn(*a, **k)
+
+    return inner
+
+
 # the same from any stop number
+@count_called
 def get_departures_from_unit(
-    bus_stop: BusStop, time: datetime.time, min_ahead: int = 60
+    bus_stop: BusStop, time: datetime.time, min_ahead: int, unique_routes: bool
 ) -> Iterable[TableData]:
-    time = utils.add_minutes_to_hour(time, 1) # TODO add to have a minute for change
-    bus_stop_ids=list(stop.id for stop in get_sibling_bus_stops(bus_stop)) 
-    return get_outgoing_tables(bus_stop_ids, time, min_ahead)
+    time = utils.add_minutes_to_hour(time, 1)
+    # add 1 to have a minute for change TODO do it better
+    bus_stop_ids = list(stop.id for stop in get_bus_stops_of_same_unit(bus_stop))
+    return get_outgoing_tables(bus_stop_ids, time, unique_routes, min_ahead)
+
+
+def cache_departures(get_departres):
+    """we need to cache function that returns a time section"""
+    cache: Dict[BusStop, CachedTimeTables] = {}
+
+    def inner(bus_stop: BusStop, time: datetime.time, min_ahead, unique_routes):
+        if bus_stop in cache and cache[bus_stop].time_valid(time):
+            return cache[bus_stop].elements_from(time)
+        query_from = add_minutes_to_hour(time, MIN_BACK)
+        query_to = add_minutes_to_hour(time, min_ahead)
+        elements = get_departres(bus_stop, query_from, min_ahead, unique_routes)
+        if bus_stop not in cache:
+            cache[bus_stop] = CachedTimeTables(query_from, query_to, set(elements))
+        else:
+            cache[bus_stop].extend(elements, query_from, query_to)
+        return cache[bus_stop].elements_from(time)
+
+    return inner
+
+
+@dataclass
+class CachedTimeTables:
+    queried_from: datetime.time
+    queried_to: datetime.time
+    elements: Set[TableData]
+
+    def time_valid(self, time):
+        if self.queried_from > time:
+            return False
+        if self.queried_to < utils.add_minutes_to_hour(time, MINIMUM_MIN_BUFFER):
+            return False
+        return True
+
+    def new_time_span(self, time):
+        if self.queried_from > time:
+            return (time, self.queried_from)
+        else:
+            return self.queried_to, add_minutes_to_hour(self.queried_to, MIN_AHEAD)
+
+    def elements_from(self, time):
+        # TODO use a sorted structure
+        return filter(lambda obj: obj.time >= time, self.elements)
+
+    def extend(self, elems, queried_from, queried_to):
+        if self.queried_from > queried_from:
+            self.queried_from = queried_from
+        if self.queried_to < queried_to:
+            self.queried_to = queried_to
+
+        for elem in elems:
+            self.elements.add(elem)
+
+
+get_departures_from_unit_cached = cache_departures(get_departures_from_unit)
 
 
 def get_route_of_one_course(
